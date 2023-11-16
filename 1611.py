c3efb5349759ebf5ccc7e6c91ad7100d48afa011
mauriciobarbosa22 <145697140+mauriciobarbosa22@users.noreply.github.com>
2023-11-16 11:01:37 +0000

    yes

---
+++ 1611.py
@@ -0,0 +1,3 @@
+'''opção m para imprimir apenas as most common 20 (as 20 palavras que mais ocorreram (counter.mostcommon))
+criar um regex que funcione como lista negra (se a minha entidade fizesse matching ocm essa regex, deitava fora das entidades ( por exemplo, se tem um dígito, deito fora; se tem um stopword deito fora; etc))
+estudar o módulo json'''
--- aula0911.py
+++ aula0911.py
@@ -2,13 +2,14 @@
 #expressao regular que permita descrever um nome próprio, imprimindo para fora o resultado
 
 import re
+from collections import Counter
 livro = 'Camilo-A_Brasileira_de_Prazins.md'
 texto1 = open(livro, 'r', encoding='utf-8').read()
 
 #texto1 = "O médico João de Deus Martins conseguiu fazer com que a paciente Margarida Carneiro da Silva Lopes tivesse alta do hospital Santa Maria no Porto."
 
 def nomesProp(texto):
-    pattern = r'\b([A-Z]\w+(?:\s(?:de|da)?\s?[A-Z]\w+)*)(?=\s|\b)'  #\b word boundary   #() captura   # ? é opcional (1 ou nenhum)    \s espaço
+    pattern = r'\b([A-ZÁÉÍÓÚÊÔ]\w+(?:\s(?:de|da|do)?\s?[A-Z]\w+)*)(?=\s|\b)'  #\b word boundary   #() captura   # ? é opcional (1 ou nenhum)    \s espaço
     matches = re.findall(pattern, texto)
 
     return matches
