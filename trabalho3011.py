f74c4786c4b00ec80f7ce65c4d7b22514c071f46
mauriciobarbosa22 <145697140+mauriciobarbosa22@users.noreply.github.com>
2023-11-30 03:50:33 +0000

    kkk

--- trabalho3011.py
+++ trabalho3011.py
@@ -11,7 +11,7 @@ def publis(texto):
 
 publis(texto)
 
-#extrair lista de # e respetivo numero de ocorrências
+#Extrair lista de # e respetivo numero de ocorrências
 
 def cardinal(texto):
     linhas = texto.split('\n')
@@ -28,4 +28,30 @@ contagem = cardinal(texto)
 
 with open('lista_de_pubs.txt', 'w', encoding='utf-8') as f:
       for linha, ocorrencias in contagem.items():
-            f.write(f'{linha}: {ocorrencias}\n')
+            f.write(f'{linha}: {ocorrencias} publicações\n')
+
+
+#Que gama de datas estão incluídas no ficheiro
+
+#fazer uma expressao regular que me encontre as mais variadas gamas de datas (tenho que especificar todas), posso, ou nao, retirar as linhas do #DATE
+
+
+def datas(texto):
+    paragrafos = texto.split('\n')
+    padrao = (r'\b\d{1,2}/\b\d{1,2}/\b\d{2,4}\b|\b\d{1,2}\b\s\bde\b\s\b\w*\b\s\bde\b\sd{2,4}|\b\d{1,2}-\b\d{1,2}-\b\d{2,4}\b|\b\d{1,2}/b/\b\d{1,2}/\b\d{1,2}|\b\d{1,2}\b\s\bde\b\s\w*\s(?:\bde\b\s\d{2,4}\b)?')
+    datas_encontradas = re.findall(padrao, texto)
+    return datas_encontradas
+
+lista_datas = datas(texto)
+
+with open('gamas_de_datas.txt', 'w', encoding='utf-8') as f:
+     for data in lista_datas:
+        f.write(f'{data}\n')
+
+
+
+
+#Saber quais são as palavras mais utilizadas, pode permitir saber qual é o tema mais comum (?)
+
+
+
