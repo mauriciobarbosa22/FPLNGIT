1011bf3685972f10a1162024d36c532ae806171a
mauriciobarbosa22 <145697140+mauriciobarbosa22@users.noreply.github.com>
2023-12-06 18:07:53 +0000

    s
    s

--- aula0911.py
+++ aula0911.py
@@ -1,2 +1,8 @@
 #nome próprio em português (maísculas, com possibilidade de 'de' pelo meio)
 #expressao regular que permita descrever um nome próprio, imprimindo para fora o resultado
+
+import re 
+livro = "Camilo-A_Brasileira_de_Prazins.md"
+open = open(livro, 'w', encoding='utf=8').read
+
+print(livro)
